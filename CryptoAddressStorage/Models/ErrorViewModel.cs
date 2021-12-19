92cd6f532e3d9cfc11945fe00542c4d29dcb4d8b
ICARUS-2 <60612247+ICARUS-2@users.noreply.github.com>
2021-12-19 17:16:03 -0500

    Init.

---
+++ CryptoAddressStorage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CryptoAddressStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
